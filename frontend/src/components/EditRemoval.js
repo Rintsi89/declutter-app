1aff2cccf0cadbf6f930f27029eac26a1060f4ec
Rintsi89 <ville.rintala@hotmail.com>
2020-01-03 14:29:35 +0200

    Add missing files to commit

---
+++ frontend/src/components/EditRemoval.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
+import { Image, Item, Icon } from 'semantic-ui-react'
+import { withRouter } from "react-router"
+import { updateImage, deleteImage } from '../reducers/removalReducer'
+import Header from './Header'
+import Title from './Title'
+import PictureForm from './PictureForm'
+import classes from '../styles/User.module.css'
+
+const EditRemoval = (props) => {
+    
+    const [form, setForm] = useState(null)
+
+    if (!props.removal) {
+        return null
+    }
+
+    const deleteImage = (event, id) => {
+        event.preventDefault()
+        if (confirm('Are you sure you want to delete this picture')) {
+            props.deleteImage(id)
+            setForm(null)
+        }
+    }
+
+    return(
+        <div>
+            <div>
+                <Header />
+                <Title title={props.removal.name} />
+            </div>
+            <div className={classes.infoarea}>
+                <Item.Group>
+                    <Item>
+                    <Item.Image size='small' src={props.removal.image} />
+                    <Item.Content className={classes.content}>
+                        <Item.Header as='a'>{props.removal.name}</Item.Header>
+                        <Item.Meta>{props.removal.note}</Item.Meta>
+                        <Item.Description>Name: {props.removal.name}</Item.Description>
+                        <Item.Description>Locations: {props.removal.name}</Item.Description>
+                        <Item.Extra>You have currently {props.removal.name} removals</Item.Extra>
+                    </Item.Content>
+                    <Item.Content>
+                        <Item.Header as='a'>Actions</Item.Header>
+                        <Item.Meta>What you would like to do with your account?</Item.Meta>
+                        <div>
+                            <Item.Description><Icon name='edit' /><button onClick={() => setForm('editform')}>Edit removal details</button></Item.Description>
+                        </div>
+                            <Item.Description><Icon name='image outline' /><button onClick={() => setForm('imageform')}>Edit removal image</button></Item.Description>
+                    </Item.Content>
+                    </Item>
+                </Item.Group>
+            </div>
+            {!form ? null : form === 'editform' ? <h3>Jeee</h3> :
+             form === 'imageform' ?
+             <PictureForm label={'Select new picture'} title={'Edit removal picture'} id={props.removal.id} delete={deleteImage}/> :
+             <EditForm />}
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+      logged_user: state.logged_user,
+      removals: state.removals
+    }
+  }
+
+const mapDispatchToProps = {
+    updateImage,
+    deleteImage
+}
+
+const ConnectedEditRemoval = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(EditRemoval)
+
+
+export default withRouter(ConnectedEditRemoval)
